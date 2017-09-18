1b9d7140bc73c951e8ab9be006f329834da858ed
chrisymassie <chrisymassie@gmail.com>
2017-09-18 11:11:14 -0500

    added annotate gem, ran annotate:install, then annotated all the things

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id           :integer          not null, primary key
+#  name         :string(255)
+#  description  :text(65535)
+#  inventory    :integer
+#  price        :decimal(7, 2)
+#  restocked_at :datetime
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  qty_sold     :integer
+#  ref_num      :string(255)
+#  style_id     :integer
+#
+
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase

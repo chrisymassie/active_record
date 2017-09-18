1d5e036993a9fe0558cda08f8c93379fadc3e04f
chrisymassie <chrisymassie@gmail.com>
2017-09-18 01:38:36 -0500

    changed migration to use #up and #down

--- db/migrate/20170918052723_add_style_to_products.rb
+++ db/migrate/20170918052723_add_style_to_products.rb
@@ -1,5 +1,10 @@
 class AddStyleToProducts < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_reference :products, :style, index: true
+  end
+
+  def down
+    remove_reference :products, :style, index: true
+    # raise ActiveRecord::IrreversibleMigration
   end
 end

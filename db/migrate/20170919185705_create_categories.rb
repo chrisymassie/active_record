fe33e25fb50fb25f1b7c424f1e04c318c34857a9
chrisymassie <chrisymassie@gmail.com>
2017-09-19 14:05:16 -0500

    wrote migration to assign category ids to existing products

--- db/migrate/20170919185705_create_categories.rb
+++ db/migrate/20170919185705_create_categories.rb
@@ -2,8 +2,25 @@ class CreateCategories < ActiveRecord::Migration[5.1]
   def change
     create_table :categories do |t|
       t.string :name
-
       t.timestamps
     end
+  end
+
+  add_reference :products, :category, index: true
+
+  reversible do |dir|
+    dir.up do
+      say "Creating initial categories"
+      cat1 = Category.create(name: "Furniture")
+      cat2 = Category.create(name: "Clothes")
+
+      say "Assigning products to categories"
+      Product.where.not(name: "Green shirt").update_all(category_id: cat1.id)
+      Product.where(name: "Green shirt").update_all(category_id: cat2.id)
+    end
+    dir.down do
+      # data will be deleted when table and column are removed
+    end
+
   end
 end

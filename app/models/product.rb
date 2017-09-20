2fb4c86db43b19af99a754111b07a42fa199658c
chrisymassie <chrisymassie@gmail.com>
2017-09-20 11:34:16 -0500

    touch products and generate migration for counter cache

--- app/models/product.rb
+++ app/models/product.rb
@@ -24,7 +24,7 @@
 
 class Product < ApplicationRecord
 
-  belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
+  belongs_to :category, class_name: 'Category', foreign_key: 'category_id', touch: true
 
   default_scope { order("name ASC") }
 

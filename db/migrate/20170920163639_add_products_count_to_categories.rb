74ca93a388ae8fb8635508f89e672edda9358352
chrisymassie <chrisymassie@gmail.com>
2017-09-20 11:41:41 -0500

    wrote migration for counter_cache w/ existing products

--- db/migrate/20170920163639_add_products_count_to_categories.rb
+++ db/migrate/20170920163639_add_products_count_to_categories.rb
@@ -1,5 +1,17 @@
 class AddProductsCountToCategories < ActiveRecord::Migration[5.1]
   def change
-    add_column :categories, :products_count, :integer
+    add_column :categories, :products_count, :integer, default: 0
+
+    # necessary code for existing products to handle count
+    reversible do |dir|
+      dir.up do
+        Category.all.each do |category|
+          count = category.products.size
+          # must use update_attribute b/c it is marked read-only
+          category.update_attribute(:products_count, count)
+        end
+      end
+    end
+
   end
 end

c175e778b260239a07b6542f216369d03aaff258
chrisymassie <chrisymassie@gmail.com>
2017-09-19 23:03:16 -0500

    added callback that assigns prefix=GEN if prefix is blank

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,4 +12,17 @@
 class Category < ApplicationRecord
   has_many :products
 
+  validates :name,   presence: true
+  validates :prefix, presence: true
+
+  before_validation :add_default_prefix
+
+  private
+
+    def add_default_prefix
+      if prefix.blank?
+        puts "*** Adding default prefix ***"
+        self.prefix = 'GEN'
+      end
+    end
 end

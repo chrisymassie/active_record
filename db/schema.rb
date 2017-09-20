be3fa330b032ff7a4fee8616684fd15875ed8358
chrisymassie <chrisymassie@gmail.com>
2017-09-19 21:21:15 -0500

    migrated prefix to categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170919192132) do
+ActiveRecord::Schema.define(version: 20170920021917) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "prefix"
   end
 
   create_table "mailing_lists_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|

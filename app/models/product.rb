55aaf6a8a59364dd6495d03a4cd43714633b9810
chrisymassie <chrisymassie@gmail.com>
2017-09-20 00:51:00 -0500

    made belongs_to explicit

--- app/models/product.rb
+++ app/models/product.rb
@@ -24,7 +24,7 @@
 
 class Product < ApplicationRecord
 
-  belongs_to :category
+  belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
 
   default_scope { order("name ASC") }
 
